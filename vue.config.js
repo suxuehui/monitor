247ab44665ce2abaca2c5aabe62da5132522b832
zy12121026 <190840024@qq.com>
2018-09-04 15:42:11 +0800

    9-4 提测下发配置

--- vue.config.js
+++ vue.config.js
@@ -17,8 +17,8 @@ module.exports = {
     proxy: {
       '/api': {
         // target: 'http://192.168.1.240:3000/mock/11', // mock地址
-        // target: 'http://192.168.6.234:3035/monitor/', // 测试环境地址
-        target: 'http://192.168.6.160:5555/monitor/', // 开发环境地址
+        target: 'http://192.168.6.234:3035/monitor/', // 测试环境地址
+        // target: 'http://192.168.6.160:5555/monitor/', // 开发环境地址
         changeOrigin: true,
         pathRewrite: {
           '^/api': '',
