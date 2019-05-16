4ac71ecc20854cf6280031caa9898528772062c8
庄源 <190840024@qq.com>
2019-05-16 18:22:01 +0800

    5-16 1

--- src/views/permission/role/components/setModal.tsx
+++ src/views/permission/role/components/setModal.tsx
@@ -32,7 +32,7 @@ export default class SetModal extends Vue {
 
   menuList: any = [];
 
-  checkList: number[] = []; // 权限选中项
+  checkedList: number[] = []; // 权限选中项
 
   checkBoxMainArr: any = []; // 权限第一级数组集合
 
@@ -93,7 +93,7 @@ export default class SetModal extends Vue {
     if (trees) {
       trees.setCheckedKeys([]);
     }
-    this.checkList = [];
+    this.checkedList = [];
     let arr: any = [];
     roleInfo({ roleId: this.data.roleId }).then((res) => {
       const { result: { resultCode, resultMessage }, entity } = res;
@@ -103,11 +103,11 @@ export default class SetModal extends Vue {
           arr.forEach((item: any, index: number) => {
             // 设置>0：取消对首页的验证
             if (this.checkBoxMainArr.indexOf(item) === -1) {
-              this.checkList.push(item);
+              this.checkedList.push(item);
             }
           });
           if (trees) {
-            trees.setCheckedKeys(this.checkList);
+            trees.setCheckedKeys(this.checkedList);
           }
         }
       } else {
@@ -115,6 +115,144 @@ export default class SetModal extends Vue {
       }
     });
   }
+
+  ListIdGroup: any = [
+    // 车辆监控列表
+    {
+      priKey: 26,
+      keys: [25, 26, 27, 28, 29, 154, 155, 156, 157, 158],
+    },
+    // 轨迹列表
+    {
+      priKey: 31,
+      keys: [31, 32],
+    },
+    // 追踪记录列表
+    {
+      priKey: 33,
+      keys: [33, 35],
+    },
+    // 追踪设备列表
+    {
+      priKey: 34,
+      keys: [34, 36, 37, 38],
+    },
+    // 电子围栏
+    {
+      priKey: 39,
+      keys: [39, 40, 41],
+    },
+    // 设备管理
+    {
+      priKey: 66,
+      keys: [66, 67, 68, 69, 71, 72, 73, 78, 150, 151, 152, 153],
+    },
+    // 设备配置
+    {
+      priKey: 131,
+      keys: [131, 132, 133, 134, 135, 136, 137, 138, 139, 140],
+    },
+    // 商户管理
+    {
+      priKey: 79,
+      keys: [79, 80, 81, 82, 83, 84],
+    },
+    // 通知公告
+    {
+      priKey: 86,
+      keys: [86, 87, 88, 89],
+    },
+    // 告警消息
+    {
+      priKey: 90,
+      keys: [90, 91, 92, 93],
+    },
+    // 行驶统计
+    {
+      priKey: 94,
+      keys: [94, 95],
+    },
+    // 成员管理
+    {
+      priKey: 96,
+      keys: [96, 97, 98, 99, 100, 101],
+    },
+    // 角色
+    {
+      priKey: 103,
+      keys: [103, 104, 105, 106, 107, 109],
+    },
+    // 系统配置
+    {
+      priKey: 111,
+      keys: [110, 111],
+    },
+    // 配置文件
+    {
+      priKey: 114,
+      keys: [114, 115, 116, 117, 118],
+    },
+  ]
+
+  boxCheck(data: any, node: any) {
+    const trees: any = this.$refs.tree;
+    const checkedArr: any = node.checkedKeys;
+    let targetId: any = 0;
+    // 判断是否在上述约束条件中：
+    this.ListIdGroup.forEach((item: any) => {
+      if (item.keys.indexOf(data.id) > -1) {
+        targetId = item.priKey;
+        this.contactList(checkedArr, data.id, targetId);
+      } else {
+        trees.setCheckedKeys(checkedArr);
+      }
+    });
+  }
+
+  contactList(checkedArr: any, clickId: number, targetId: number) {
+    const trees: any = this.$refs.tree;
+    if (checkedArr.indexOf(clickId) <= -1) {
+      // 删除
+      this.ListIdGroup.forEach((item: any) => {
+        if (item.priKey === clickId) {
+          if (this.getArrEqual(item.keys, checkedArr).length > 0) {
+            const arr = lodash.cloneDeep(checkedArr);
+            arr.push(targetId);
+            setTimeout(() => {
+              trees.setCheckedKeys(arr);
+            }, 10);
+          } else {
+            setTimeout(() => {
+              trees.setCheckedKeys(checkedArr);
+            }, 10);
+          }
+        } else {
+          trees.setCheckedKeys(checkedArr);
+        }
+      });
+    } else {
+      // 新增
+      const arr = lodash.cloneDeep(checkedArr);
+      arr.push(targetId);
+      setTimeout(() => {
+        trees.setCheckedKeys(arr);
+      }, 10);
+    }
+  }
+
+  // 找出两个数组相同元素并返回
+  getArrEqual(arr1: any, arr2: any) {
+    const newArr = [];
+    for (let i = 0; i < arr2.length; i+=1) {
+      for (let j = 0; j < arr1.length; j+=1) {
+        if (arr1[j] === arr2[i]) {
+          newArr.push(arr1[j]);
+        }
+      }
+    }
+    return newArr;
+  }
+
 
   // 设置权限列表
   setList(list: any) {
@@ -124,7 +262,8 @@ export default class SetModal extends Vue {
         ref="tree"
         show-checkbox
         node-key="id"
-        default-checked-keys={this.checkList}
+        default-checked-keys={this.checkedList}
+        on-check={this.boxCheck}
         props={
           {
             children: 'list',
@@ -147,7 +286,7 @@ export default class SetModal extends Vue {
     From.resetFields();
     this.loading = false;
     this.checkBoxMainArr = [];
-    this.checkList = [];
+    this.checkedList = [];
   }
 
   onSubmit() {
