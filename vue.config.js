bd4b39b4a6c6319e225e8bc03cfbef079ec7de04
庄源 <190840024@qq.com>
2019-09-03 08:40:29 +0800

    打开无权限操作提示

--- vue.config.js
+++ vue.config.js
@@ -70,6 +70,7 @@ module.exports = {
         },
       },
       '/rootApi': {
+        // target: 'http://192.168.3.83:5555/', // 开发环境地址
         target: 'http://192.168.6.194:5555/', // 开发环境地址
         // target: 'http://test-monitor-blacktea.mysirui.com/api/', // 测试环境地址
         // target: 'http://pre-monitor-blacktea.mysirui.com/api/', // 预发布环境地址
