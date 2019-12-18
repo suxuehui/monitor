df73639249c15152d81165809b9ed842e0cdc902
admin <admin@maningdeMac-mini-2.local>
2019-12-18 11:09:15 +0800

    hhh

--- vue.config.js
+++ vue.config.js
@@ -60,10 +60,10 @@ module.exports = {
     proxy: {
       '/api': {
         // target: 'http://192.168.3.83:5555/monitor/', // 开发环境地址/
-         target: 'http://192.168.6.194:5555/monitor/', // 开发环境地址/
+        // target: 'http://192.168.6.194:5555/monitor/', // 开发环境地址/
         // target: 'http://test-monitor-blacktea.mysirui.com/api/monitor/', // 测试环境地址
         //target: 'http://pre-monitor-blacktea.mysirui.com/api/monitor/', // 预发布环境地址
-        // target: 'https://monitor-blacktea.mysirui.com/api/monitor/', // 正式环境地址
+         target: 'https://monitor-blacktea.mysirui.com/api/monitor/', // 正式环境地址
         changeOrigin: true,
         pathRewrite: {
           '^/api': '',
@@ -73,8 +73,8 @@ module.exports = {
         // target: 'http://192.168.3.83:5555/', // 开发环境地址
         // target: 'http://192.168.6.194:5555/', // 开发环境地址
         // target: 'http://test-monitor-blacktea.mysirui.com/api/', // 测试环境地址
-        target: 'http://pre-monitor-blacktea.mysirui.com/api/', // 预发布环境地址
-        // target: 'https://monitor-blacktea.mysirui.com/api/', // 正式环境地址
+         //target: 'http://pre-monitor-blacktea.mysirui.com/api/', // 预发布环境地址
+        target: 'https://monitor-blacktea.mysirui.com/api/', // 正式环境地址
         changeOrigin: true,
         pathRewrite: {
           '^/rootApi': '',
